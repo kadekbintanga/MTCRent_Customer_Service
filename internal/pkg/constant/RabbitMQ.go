031f9cfed759a8497252670049f954310c756f73
yuswa-arba <yuswa98@gmail.com>
2025-01-23 12:26:15 +0800

    - Change rabbitmq concept - Add migration to root.go file

--- internal/pkg/constant/RabbitMQ.go
+++ internal/pkg/constant/RabbitMQ.go
@@ -1,9 +1,9 @@
 package constant
 
+/** --- EXCHANGE --- */
+
+const RABBITMQ_EXCHANGE_SERVICE_DOMAIN_FEATURE_ACTION = "service.domain.feature.action.exchange"
+
 /** --- QUEUE --- */
 
-const RABBITMQ_QUEUE_DEFAULT = "default"
-
-/** --- KEY --- */
-
-const RABBITMQ_KEY_TESTING_MESSAGE = "services-testing-message"
+const RABBITMQ_QUEUE_SERVICE_DOMAIN_FEATURE_ACTION = "service.domain.feature.action.queue"
